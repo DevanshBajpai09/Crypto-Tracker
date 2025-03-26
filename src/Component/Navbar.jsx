f10cacae78a33a42e11061b258b3230d9c6eb310
DevanshBajpai09 <devanshbajpai09@gmail.com>
2025-03-27 00:13:56 +0530

    update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,20 +1,13 @@
-"use client"
+
 
 import React from 'react'
 
 import { IoIosArrowForward } from "react-icons/io";
 import { Button } from  "../components/ui/Button";
-// import { BlurFade } from "@/components/magicui/blur-fade";
-import { MdOutlineLightMode } from "react-icons/md";
-import { MdDarkMode } from "react-icons/md";
-// import { Toggle } from "@/components/ui/toggle"
-// import { useAuth } from '../Context/AuthContext';
+
 
 const Navbar = () => {
-//   const{user} = useAuth()
-//   const toggleColor = () => {
-//     setToggle(!toggle); // Toggle state from parent component
-//   };
+
 
 
   return (
@@ -23,22 +16,22 @@ const Navbar = () => {
   
     <div className="flex items-center justify-between py-3 px-10">
         
-    <Button  className="flex items-center gap-1">
-    <a href="/singup">
+  
+    <a href="/singup" className=' className="flex items-center gap-1"'>
       <h3 className="text-3xl font-semibold">
         Crypto<span className="text-blue-500">Predicition</span>
       </h3>
     </a>
-    </Button>
+   
 
     
 
 
-    <Button href="/auth/signup">
-      <Button className="bg-black text-white rounded-none px-4 py-2 ml-2">
+   
+      <Button  className="bg-black text-white rounded-none px-4 py-2 ml-2">
         Get Started <IoIosArrowForward />
       </Button>
-    </Button>
+  
   </div>
 
   )
